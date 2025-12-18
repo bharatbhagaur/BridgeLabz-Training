dea9eff75d6321cf638a0879ad27032cf28efce8
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2025-12-18 11:40:45 +0530

    [Bharat Singh] : AveragePercentage

---
+++ core-java-practice/gcr-codebase/programming-elements/AveragePercentage.java
@@ -0,0 +1,10 @@
+public class AveragePercentage {
+    public static void main(String[] args) {
+        int maths=85;//marks in maths
+        int physics=95;//marks in physics
+        int chemistry=90;//marks in chemistry
+        int t=maths+physics+chemistry;//total marks
+        int avg=t/3;//average marks
+        System.out.println("Sam's average marks in PCM is :"+avg);//output total marks
+    }
+}
