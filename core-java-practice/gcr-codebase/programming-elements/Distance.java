fa101d46346114b5de70b92154984faeecc62f7e
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2025-12-18 14:53:20 +0530

    [Bharat Singh] : Distance

---
+++ core-java-practice/gcr-codebase/programming-elements/Distance.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int feet = sc.nextInt();
+        // input distance in feet
+        double yards = feet / 3.0;
+        // conversion to yards
+        double miles = yards / 1760.0;
+        // conversion to miles
+        System.out.println("Distance in yards is"+ yards +" while in miles is "+ miles);
+    }
+}
