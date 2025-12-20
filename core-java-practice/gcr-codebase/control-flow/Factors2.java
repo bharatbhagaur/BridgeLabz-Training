f912db16807c4dbbcae2d14750fec6e5fe124b4d
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2025-12-20 10:51:24 +0530

    [Bharat Singh] Add . Factors2

---
+++ core-java-practice/gcr-codebase/control-flow/Factors2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Factors2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number <= 0) {
+            //negative numbers and zero do not have factors
+            System.out.println("Please enter a positive integer");
+        } else {
+            System.out.println("Factors of " + number + " are:");
+            //using while loop to find factors
+            int i = 1;
+            while (i < number) {
+                //check for factors
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+                i++;
+            }
+        }
+    }
+}
