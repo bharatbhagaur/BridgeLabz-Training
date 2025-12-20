da53d907ca7a91dca09d905d3dc8ead9d2f3621a
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2025-12-20 10:35:05 +0530

    [Bharat Singh] Add . BonusEmployee

---
+++ core-java-practice/gcr-codebase/control-flow/BonusEmployee .java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BonusEmployee {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter employee's salary: ");
+    int salary=sc.nextInt();
+    System.out.print("Enter employee's years of service: ");
+    int years=sc.nextInt();
+    if(years>5) {
+        double bonus=0.05*salary;
+        //5% bonus for more than 5 years
+        System.out.println("Employee's bonus is: "+bonus);
+    } else {
+        System.out.println("Employee is not eligible for bonus");
+    }
+}    
+}
