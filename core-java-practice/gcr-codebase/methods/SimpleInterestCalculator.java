b2ffb76a6c0a67ffcf549e44d5558365ec3ef4d3
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2025-12-22 15:45:18 +0530

    [Bharat Singh] : Add . SimpleInterestCalculator

---
+++ core-java-practice/gcr-codebase/methods/SimpleInterestCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+
+    // Method to calculate simple interest
+    static double calculateSI(double principalAmt, double interestRate, double timePeriod) {
+        return (principalAmt * interestRate * timePeriod) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter Time: ");
+        double time = sc.nextDouble();
+
+        // Calling method
+        double simpleInterest = calculateSI(principal, rate, time);
+
+        // Output
+        System.out.println("The Simple Interest is " + simpleInterest +
+                " for Principal " + principal +
+                ", Rate of Interest " + rate +
+                " and Time " + time);
+    }
+}
