2ab8095ff071b337f4d09a38b095ea968216d890
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2026-07-31 14:16:21 +0530

    [Added solution of CamelCase

---
+++ Refresher-Training/Refresher-Training/Day-01/CamelCase.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class CamelCase {
+
+    public static int camelcase(String s) {
+        int count = 1;
+
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'A' && ch <= 'Z') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int result = camelcase(s);
+        System.out.println(result);
+
+        sc.close();
+    }
+}
