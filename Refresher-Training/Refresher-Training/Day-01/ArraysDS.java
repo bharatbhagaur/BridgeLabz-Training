0e9f70b7b26fa5a9b5a20a50e9ce7229845dd3ff
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2026-07-31 14:19:11 +0530

    [Added solution of ArraysDS

---
+++ Refresher-Training/Refresher-Training/Day-01/ArraysDS.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ArraysDS {
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+        List<Integer> reverse = new ArrayList<>();
+
+        for (int i = a.size() - 1; i >= 0; i--) {
+            reverse.add(a.get(i));
+        }
+
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            a.add(sc.nextInt());
+        }
+
+        List<Integer> result = reverseArray(a);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
