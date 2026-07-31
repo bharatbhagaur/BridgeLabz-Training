f403194e730dc44d74924dfa152055effeda9076
bharatbhagaur <131939066+bharatbhagaur@users.noreply.github.com>
2026-07-31 14:35:23 +0530

    [Added solution of class name is solution (java String tokens)

---
+++ Refresher-Training/Refresher-Training/Day-01/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+
+        if (s.length() == 0) {
+            System.out.println(0);
+            scan.close();
+            return;
+        }
+
+        String[] tokens = s.split("[ !,?._'@]+");
+
+        System.out.println(tokens.length);
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+
+        scan.close();
+    }
+}
