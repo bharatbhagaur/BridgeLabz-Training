4c1c30bd4a4b3e6fd9c990db7916e8147e92980e
bharatbhagaur <bharatbhagaur123@gmail.com>
2026-08-17 12:23:53 +0530

    [Bharat Singh] Added Day-12 Opps Scenario Based problem of Class Instance

---
+++ Refresher-Training/Refresher-Training/Day-12/Opps-Scenario-Based-Problem/Problem1_Class_Instance/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Person p = new Person(15);
+
+        p.amIOld();
+
+        p.yearPasses();
+
+        p.amIOld();
+    }
+}
